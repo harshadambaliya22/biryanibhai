67d23103f295f14f8fc5fc3b436f04b4dbb8fc09
HardikDesigner2024 <hardik.amazingwebinfosys@gmail.com>
2024-07-22 19:39:10 +0530

    added section in home page

---
+++ assets/js/cutom.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+	$(".bb-testimonial-silder").slick({
+		dots: true,
+		arrows: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
+});
